8105c695b9ae2a4fd59a68c8302bf9d069e4db5d
rocky566 <rockkhanboy45@gmail.com>
2024-12-31 17:08:58 +0530

    Update Offbot.js

--- Priyansh/commands/Offbot.js
+++ Priyansh/commands/Offbot.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "offbot",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝗠𝗿 𝗦𝗮𝗵𝗶𝗹",
 	description: "turn the bot off",
 	commandCategory: "system",
 	cooldowns: 0
