d1b8a576f401981ef4ddf7298c588480ed7eec07
rocky566 <rockkhanboy45@gmail.com>
2024-12-31 17:08:00 +0530

    Update Obama.js

--- Priyansh/commands/Obama.js
+++ Priyansh/commands/Obama.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "obama",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝗠𝗿 𝗦𝗮𝗵𝗶𝗹",
 	description: "Obama Tweet post",
 	commandCategory: "edit-img",
 	usages: "[text]",
