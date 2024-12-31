e5d6f49ea3f1bbdd9f3dc3478c94c2d67f41f014
rocky566 <rockkhanboy45@gmail.com>
2024-12-31 17:06:37 +0530

    Update Bio.js

--- Priyansh/commands/Bio.js
+++ Priyansh/commands/Bio.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "bio",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝗠𝗿 𝗦𝗮𝗵𝗶𝗹",
 	description: "Change bot's bio",
 	commandCategory: "admin",
 	usages: "bio [text]",
