b03bb36a5a88db2b3eedd4daf158bfd5f5ec0cac
rocky566 <rockkhanboy45@gmail.com>
2024-12-31 17:09:47 +0530

    Update Rank.js

--- Priyansh/commands/Rank.js
+++ Priyansh/commands/Rank.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "rank",
 	version: "2.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝗠𝗿 𝗦𝗮𝗵𝗶𝗹",
 	description: "View Member Rankings",
 	commandCategory: "Group",
 	usages: " [user] or [tag]",
