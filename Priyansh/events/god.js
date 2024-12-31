6d1721595d5700cf460ee71e00eff13829db575a
rocky566 <rockkhanboy45@gmail.com>
2024-12-31 16:37:29 +0530

    Update god.js

--- Priyansh/events/god.js
+++ Priyansh/events/god.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "god",
 	eventType: ["log:unsubscribe","log:subscribe","log:thread-name"],
 	version: "1.0.0",
-	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+	credits: "𝙈𝙧 𝙎𝙖𝙝𝙞𝙡",
 	description: "Record bot activity notifications!",
     envConfig: {
         enable: true
