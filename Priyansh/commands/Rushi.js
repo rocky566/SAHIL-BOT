7b4f69648d3fa377646474a57720f5449f2266c5
rocky566 <rockkhanboy45@gmail.com>
2024-12-31 17:12:48 +0530

    Update Rushi.js

--- Priyansh/commands/Rushi.js
+++ Priyansh/commands/Rushi.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "rushia",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+ credits: "𝗠𝗿 𝗦𝗮𝗵𝗶𝗹",
  description: "Random Rushia",
  commandCategory: "random-img",
  usages: "rushia",
