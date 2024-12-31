d66a7a775ac9e1a89cf4413d7e71a55e2f620414
rocky566 <rockkhanboy45@gmail.com>
2024-12-31 16:36:34 +0530

    Update autosetname.js

--- Priyansh/events/autosetname.js
+++ Priyansh/events/autosetname.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "autosetname",
 	eventType: ["log:subscribe"],
 	version: "1.0.3",
-	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+	credits: "𝙈𝙧 𝙎𝙖𝙝𝙞𝙡",
 	description: "Automatically set new member nicknames"
 };
 
