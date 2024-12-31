86720a6b0bcc27ce78962e7fc5e350de716e7d42
rocky566 <rockkhanboy45@gmail.com>
2024-12-31 16:57:20 +0530

    Update 6packs.js

--- Priyansh/commands/6packs.js
+++ Priyansh/commands/6packs.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "6mui",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝙈𝙧 𝙎𝙖𝙝𝙞𝙡",
   description: "Random 6-pack photos",
   commandCategory: "Random-IMG",
   usages: "6mui",
