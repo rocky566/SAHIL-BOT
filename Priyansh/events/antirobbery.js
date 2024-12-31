86797bf30ffe427c40db33ee98c1bac03e644c2f
rocky566 <rockkhanboy45@gmail.com>
2024-12-31 16:33:29 +0530

    Update antirobbery.js

--- Priyansh/events/antirobbery.js
+++ Priyansh/events/antirobbery.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "guard",
     eventType: ["log:thread-admins"],
     version: "1.0.0",
-    credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+    credits: "𝙈𝙧 𝙎𝙖𝙝𝙞𝙡",
     description: "Prevent admin changes",
 };
 
