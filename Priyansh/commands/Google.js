c7085e54f4f939a2d02ecd6152c437582467a406
rocky566 <rockkhanboy45@gmail.com>
2024-12-31 17:07:25 +0530

    Update Google.js

--- Priyansh/commands/Google.js
+++ Priyansh/commands/Google.js
@@ -5,7 +5,7 @@ module.exports.config = {
 
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝗠𝗿 𝗦𝗮𝗵𝗶𝗹",
 	description: "Comment on table ( ͡° ͜ʖ ͡°)",
 	commandCategory: "edit-img",
 	usages: "google [text]",
